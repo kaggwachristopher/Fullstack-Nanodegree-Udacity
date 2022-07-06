509732862fe94985772dff3a486a40fb13e0b374
Christopher Kaggwa <christopher@emata.io>
2022-07-06 12:15:23 +0300

    string composittion in queries

--- demo.py
+++ demo.py
@@ -3,6 +3,9 @@ import psycopg2
 connection = psycopg2.connect(database="test_db",user="postgres")
 
 cursor = connection.cursor()
+cursor.execute('''
+drop table if exists table2;
+''')
 
 cursor.execute('''
 create table table2(
@@ -10,7 +13,16 @@ create table table2(
     completed boolean not null default False
 );
 ''')
-cursor.execute('insert into table2 (id,completed) values (1,true)')
+cursor.execute('insert into table2 (id,completed) values (%s, %s)',(1,True))
+
+sql = 'INSERT INTO table2 (id, completed) VALUES (%(id)s, %(completed)s);'
+
+data = {
+  'id': 2,
+  'completed': False
+}
+
+cursor.execute(sql, data)
 
 connection.commit()
 
