bc1a93e15146df1fc181ab5c7d3bc27909df7d08
Christopher Kaggwa <christopher@emata.io>
2022-07-06 19:49:30 +0300

    defining database models from classes

--- flask-hello-app.py
+++ flask-hello-app.py
@@ -6,6 +6,11 @@ app =  Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/example'
 db = SQLAlchemy(app)
 
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
 @app.route('/')
 def index():
     return 'Hello world'
