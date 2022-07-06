b867d3a86c3dbdd8ebcb924af1d55405187b5735
Christopher Kaggwa <christopher@emata.io>
2022-07-06 20:27:01 +0300

    interactive mode usage

--- flask_hello_app.py
+++ flask_hello_app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app =  Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Person(db.Model):
