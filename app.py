dc1edf7b48b3c096c687b9d9dae227bd7ddec72a
Christopher Kaggwa <christopher@emata.io>
2022-07-06 22:33:26 +0300

    Added migrations using Flask-Migrate

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app =  Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/example'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/migrations_example'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+migrate = Migrate(app,db)
 
 class Person(db.Model):
     __tablename__ = 'persons'
@@ -14,8 +17,15 @@ class Person(db.Model):
 
     def __repr__(self):
         return f'<Person ID: {self.id}, name:{self.name}>'
-db.create_all()
+    
+class ToDo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
 
+    def __repr__(self):
+        return f'<Person ID: {self.id}, name:{self.description}>'
+    
 @app.route('/')
 def index():
     person = Person.query.first()
