f9cd84714aa342009d4d4c384fc4a651c009b94b
Christopher Kaggwa <christopher@emata.io>
2022-07-06 23:46:02 +0300

    work with an api to update and fetch to a web page

--- app.py
+++ app.py
@@ -1,36 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import request
+import sys
 
-app =  Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/migrations_example'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
-migrate = Migrate(app,db)
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
 
-class Person(db.Model):
-    __tablename__ = 'persons'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
 
-    def __repr__(self):
-        return f'<Person ID: {self.id}, name:{self.name}>'
-    
-class ToDo(db.Model):
-    __tablename__ = 'todos'
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(), nullable=False)
+db.create_all()
 
-    def __repr__(self):
-        return f'<Person ID: {self.id}, name:{self.description}>'
-    
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():   
+   body={}
+   error = False
+   try: 
+       description =  request.get_json()['description']
+       todo = Todo(description=description)
+       body['description'] = todo.description
+       db.session.add(todo)
+       db.session.commit()
+   except:        
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+   finally:
+        db.session.close()           
+        if  error == True:
+            abort(400)
+        else:            
+            return jsonify(body)
+
 @app.route('/')
 def index():
-    person = Person.query.first()
-    return 'Hello ' + person.name
+    return render_template('index.html', data=Todo.query.all())
+
+
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host="0.0.0.0")
+   app.run(host="0.0.0.0", port=3000)
